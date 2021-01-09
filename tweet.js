c6a9a2c2cbc68d83e302ec4626bdf5c8e03aef75
99designs Applicant <user@example.com>
2021-01-10 10:00:41 +1100

    refactored functions and corrected syntax

--- tweet.js
+++ tweet.js
@@ -30,55 +30,60 @@
     "review":"So bad it's bad",
     "score":7}
 ];
-
+const createMovie =>
 const movies =
   JSON.parse(movies.json)
 
 const  reviews =
 JSON.parse(reviews.json)
 
-const movieTitle = movies_array[i].title
-const year       = movies_array[i].year
-const review     = reviews_array[i].review
-const score      = reviews.array[i].score
 
- const rating   =  Math.round(score + 5) / 10
+ const  starRating = (rating) =>
 
- function starRating($rating){
+ {
 
-
-  let stars  = '';
-
-    let full_stars = floor(score/2);
-    //10 = 5, 8 = 4, etc
-     let half_star = score%2;
-    //Should be 1 for every odd number.
-     let empty_stars = 5 - full_stars - half_star;
-
+    const score      = reviews.array[i].score
+    const rating   =  (score ) / 20
+    let stars  = '';
+    let full_stars = Math.round(rating);
+    let half_star = Math.round(rating*2) /2;
+    let empty_stars = 5 - full_stars - half_star;
     for($i = 0; $i < $full_stars; $i++){
-
-       const stars = '<span class="fa fa-star checked"></span>';
+      const stars = '<span class="fa fa-star checked"></span>';
     }
 
-    if(half_star){
-
-        const stars = '<span class="fa fa-star-half-alt checked"></span>';
+        if(half_star){
+      const stars = '<span class="fa fa-star-half-alt checked"></span>';
     }
 
     for($i = 0; $i < $empty_stars; $i++){
-
-        const stars  = '<span class="fa fa-star"></span>';
+      const stars  = '<span class="fa fa-star"></span>';
     }
     return stars;
-}
-
-const tweet = movieTitle + (year) + review + stars
-tweet.includes(year)
-console.log (tweet)
-tweet.length => 140
-if tweet.length > 140 ,console.log(truncate_string(movieTitle,25))
-
-if tweet.length > 140 && movieTitle.length =< 25 , console.log(truncate_string(tweet,140))
+};
 
 
- const ratingStars   =  Math.round(score + 5) / 20
+
+const makeTweet =(tweet) => {
+  const movieTitle = movies_array[i].title
+  const year       = movies_array[i].year
+  const review     = reviews_array[i].review
+  const tweet      =    `${movieTitle}${year}${review}` + stars
+  if (tweet.includes(`${year}`) )
+    {return tweet;}
+      return false ;
+
+  };
+
+
+
+
+const validateTweet =(tweet,movieTitle) => {
+
+ if (tweet.length =< 140) {return tweet;}
+
+  if  ( tweet.length > 140  && movieTitle.length > 25)
+   {movieTitle.length == 25 && tweet.length =< 140;}
+  if (tweet.length > 140 && movieTitle.length =< 25 ) {tweet.substring(0,139);}
+  return tweet
+};
