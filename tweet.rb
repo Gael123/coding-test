463d3f7ff6860a436d09e287db47bf4753773260
99designs Applicant <user@example.com>
2021-01-20 02:50:03 +1100

    works

--- tweet.rb
+++ tweet.rb
@@ -4,25 +4,22 @@ class Tweet
   attr_accessor :movies, :reviews
 
   def initialize
-     @movies = JSON.parse(File.read(ARGV[0]))
+    @movies = JSON.parse(File.read(ARGV[0]))
      @reviews = JSON.parse(File.read(ARGV[1]))
      @stars = []
      @ratings = []
-
-
    end
 
   def convert_mjson
-
-     @movies = movies
+   @movies = movies
      @movietitles = []
      @years    = []
      @movies.each {|movie|
       @movietitles << movie['title']
       @years << '(' + (movie['year']).to_i.to_s + ')' }
 
-       puts @movietitles.inspect
-       puts @years.inspect
+       # puts @movietitles.inspect
+       # puts @years.inspect
   end
 
 def convert_rjson
@@ -30,24 +27,25 @@ def convert_rjson
   @input = []
   @reviewtitles = []
   reviews.each { |review|
-     @input << ':' + review['review']
+   @input << ':' + review['review']
      @reviewtitles << review['title']
   }
-    puts @input.inspect
-    puts @reviewtitles.inspect
+    # puts @input.inspect
+    # puts @reviewtitles.inspect
 end
 
  def get_scores
   @reviews = reviews
    scores = []
-  reviews.each { |review|
+  @reviews.each { |review|
     scores << review['score'].to_i
   }
    @ratings = []
   scores.each {|score|
     @ratings << (score / 20.to_f).round()
-               }
 
+               }
+def convert_ratings
 full_star = '★'
   @stars = []
   @ratings.each { |num|
@@ -64,7 +62,7 @@ full_star = '★'
     }
    @stars.delete_at(4)
    @stars.flatten
-  puts @stars.inspect
+  # puts @stars.inspect
 end
 
 # def convert_rating
@@ -79,7 +77,7 @@ end
 #       end
 #     }
 #    puts @stars.inspect
-# end
+end
 
 def concat_arrays
   results = []
@@ -96,71 +94,22 @@ def concat_arrays
     item2 = @input[index]
     comments << item1 + item2
   end
- puts comments
+ # puts comments
 
+comments
 
- # = []
- # comments.each_with_index do |item1,index|
- #  item2 = @stars[index]
- # tweetmovies << item1 +item2
- # end
+ sentences = []
+  comments.each_with_index do  |item1,index|
+   item2 = @stars[index].to_s
+   sentences << item1 + item2
 
+  end
 
-
+ puts sentences.inspect
  end
 
 
-#   movies = JSON.parse(File.read(ARGV[0]))
-#   reviews = JSON.parse(File.read(ARGV[1]))
-#   comment = []
-#   movies.each { |movie| comment << movie['title'] + movie['year'].to_i.to_s}
-#   input = []
-#   reviews.each { |review|
-#     input << ' :' + review['review']
-#   }
 
-#   scores = []
-#   reviews.each { |review|
-#     scores << review['score'].to_i
-#   }
-
-#   ratings = []
-#   scores.each {|score|
-#     ratings << (score / 20.to_f).ceil
-#                }
-#   full_star = '★'
-#   stars = []
-#   ratings .each { |num|
-#       if num == 2 || 4 || 5
-#         stars << full_star * num
-#       else
-#         stars << full_star * (num- 1) + '1/2'
-#       end
-#     }
-#   stars.delete_at(2)
-#  # puts stars.inspect
-#   input.delete_at(2)
-#   results = []
-#   comment.each_with_index do |item1,index|
-#     item2 = input[index]
-#     results << item1 + item2
-#   end
-
-#   tweet = []
-#   results.each_with_index do |item1,index |
-#     item2 = stars[index]
-#     tweet << item1 + item2
-#   end
-#   tweet.each { |string|
-#         string.length <= 140
-#        if tweet.length > 140 && movie['title'].length > 25
-#          movie['title'].length == 25
-#        else
-#          tweet[0, 39]
-#        end
-#       }
-
-#   puts tweet.to_json
 
  private
 
@@ -182,7 +131,12 @@ end
 
 
 tweet = Tweet.new
-puts tweet.convert_mjson
-puts tweet.convert_rjson
+ tweet.convert_mjson
+ tweet.convert_rjson
+# puts tweet.concat_arrays
+ tweet.get_scores
+ tweet.convert_ratings
 puts tweet.concat_arrays
-puts tweet.get_scores
+
+
+
